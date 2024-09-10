64abb88729f33fc1bc0ef0903356ac94cc80d859
Omin Hemapriya <it21801372@my.sliit.lk>
2024-09-11 01:11:38 +0530

    CSS update

--- backend/data/products.js
+++ backend/data/products.js
@@ -3,15 +3,15 @@
 
 const products = [
     { id: '1', name: 'Black T-shirt', category: 'shirts', price: '2000', image: 'https://rukminim2.flixcart.com/image/850/1000/xif0q/t-shirt/o/2/u/xl-rh-1-fantoosh-original-imagusu3scwz6q6k.jpeg?q=90&crop=false' },
-    { id: '2', name: 'Blue trouser', category: 'trousers', price: '4000', image:  'https://assets.ajio.com/medias/sys_master/root/20230624/UnSL/649644f8a9b42d15c9d88da8/-473Wx593H-465246327-blue-MODEL2.jpg'},
+    { id: '2', name: 'Blue trouser', category: 'trousers', price: '4000', image:  'https://cdn.yoursclothing.com/Images/ProductImages/99f454f3-a062-4d_214452_BK.jpg'},
     { id: '3', name: 'Red shoe', category: 'shoes', price: '6000', image: 'https://static.nike.com/a/images/t_default/0adc5f31-1d41-4337-9354-8e305444037c/AIR+FORCE+1+%2707+LV8.png' },
-    { id: '4', name: 'Blue long sleeve shirt', category: 'shirts', price: '2000', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSeIlwh2Ufo69uUcIx85XBM87_6DFJNyVEPQ&s' },
-    { id: '5', name: 'white casual trouser', category: 'trousers', price: '4000', image: 'https://shopforaurelia.com/cdn/shop/files/23CRE60211-605738_2.jpg?v=1722025268&width=1500' },
-    { id: '6', name: 'Black and white Nike shoe', category: 'shoes', price: '6000', image: 'https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/488cc6e0-8fc2-4e44-bc2d-891990853cc6/W+NIKE+REVOLUTION+7+EASYON.png' },
+    { id: '4', name: 'Blue long sleeve shirt', category: 'shirts', price: '2000', image: 'https://m.media-amazon.com/images/I/61DcFl7qutL._AC_UY1000_.jpg' },
+    { id: '5', name: 'white casual trouser', category: 'trousers', price: '4000', image: 'https://4.imimg.com/data4/JN/UI/MY-28459816/trousers-1-500x500.jpg' },
+    { id: '6', name: 'Black and white Nike shoe', category: 'shoes', price: '6000', image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5b0981ff-45f8-40c3-9372-32430a62aaea/W+NIKE+DUNK+HIGH.png' },
     { id: '7', name: 'Red over sized T-shirt', category: 'shirts', price: '2000', image: 'https://slatehash.com/cdn/shop/files/VG-SH-46203006.jpg?v=1686387080' },
-    { id: '8', name: 'Black trouser', category: 'trousers', price: '4000', image: 'https://signature.lk/wp-content/uploads/2023/10/Casual_Trouser_Signature_Mens_001.jpg' },
-    { id: '9', name: 'white shoe', category: 'shoes', price: '6000', image: 'https://www.campusshoes.com/cdn/shop/products/RAISE_WHT_f1a5a2ec-8a23-4795-a796-0da7455dc57a.jpg?v=1705476016' },
-    { id: '10', name: 'Long sleeve white T-shirt', category: 'shirts', price: '2000', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWEuEUaUYC4KHQB6m__eLSQdjXbm2fs-NX2A&s' },  
+    { id: '8', name: 'Black trouser', category: 'trousers', price: '4000', image: 'https://signature.lk/wp-content/uploads/2022/12/Trousers-New-Edit_0056_Layer-1-3.jpg' },
+    { id: '9', name: 'white shoe', category: 'shoes', price: '6000', image: 'https://thursdayboots.com/cdn/shop/files/1024x1024-Men-Premier-LowTop-White-BlindEyelets-121423-3.4_2_3615bf9d-be8c-4f08-a54e-f6a5b91208ca_grande.jpg?v=1705532975' },
+    { id: '10', name: 'Long sleeve white T-shirt', category: 'shirts', price: '2000', image: 'https://bearappeal.com/wp-content/uploads/2019/06/white-min.png' },  
 ];
 
 module.exports = products;
